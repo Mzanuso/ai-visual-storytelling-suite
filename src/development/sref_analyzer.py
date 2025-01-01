d668609e969924e43f33eaae81335d651dcd75dd
Mzanuso <mzanuso@gmail.com>
2025-01-01 13:38:32 -0500

    Add core SREF handling modules

---
+++ src/development/sref_analyzer.py
@@ -0,0 +1,12 @@
+class SREFAnalyzer:
+    def __init__(self):
+        self.current_analysis = None
+        self.analysis_history = []
+
+    def analyze_variations(self, sref_id):
+        """Analyze variations of a SREF"""
+        pass
+
+    def generate_derived_prompts(self, analysis):
+        """Generate new prompts based on analysis"""
+        pass
---
+++ src/discovery/sref_explorer.py
@@ -0,0 +1,12 @@
+class SREFExplorer:
+    def __init__(self):
+        self.current_sref = None
+        self.search_filters = {}
+
+    def search_srefs(self, filters):
+        """Search SREFs based on filters"""
+        pass
+
+    def analyze_sref(self, sref_id):
+        """Analyze a specific SREF"""
+        pass
